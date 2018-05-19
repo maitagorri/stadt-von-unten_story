d53c5bc203dd3e4a7aed54af147ae4f1efdb84ef
maitagorri <maitagorri@gmail.com>
2018-05-19 17:07:56 +0200

    Added points for merchants

---
+++ data/Gewerbe_1.js
@@ -0,0 +1,13 @@
+var json_Gewerbe_1 = {
+"type": "FeatureCollection",
+"name": "Gewerbe_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Name": "Gretchen", "Picture": null }, "geometry": { "type": "Point", "coordinates": [ 13.387524399162515, 52.496048381105894 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Name": "KLAS Auto Service", "Picture": null }, "geometry": { "type": "Point", "coordinates": [ 13.387017931395128, 52.496028208949184 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Name": "A.P.O. Autowerkstatt", "Picture": null }, "geometry": { "type": "Point", "coordinates": [ 13.386639263905499, 52.495754443050217 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Name": "Prestige", "Picture": null }, "geometry": { "type": "Point", "coordinates": [ 13.387193065109086, 52.494613253152643 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Name": "Metropol Taxischule", "Picture": null }, "geometry": { "type": "Point", "coordinates": [ 13.387320865386837, 52.494232849938612 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Name": "LPG Biomarkt", "Picture": null }, "geometry": { "type": "Point", "coordinates": [ 13.388584668133486, 52.49590429406922 ] } }
+]
+}
